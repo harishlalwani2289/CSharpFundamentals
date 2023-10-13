d80d3621391aee99dda94d7342074c5ff0fdd370
Harish Lalwani <harishlalwani2289@gmail.com>
2023-10-13 16:25:12 -0400

    MAx and min of various data types

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -11,26 +11,10 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            byte number = 253;
-            float price = 20.54f;
-            char character = '$';
-            string name = "Harish";
-            bool isAdmin = false;
-            bool isGuest = false;
-            int n = 0;
-            char character1 = 'a';
-            char character2 = 'b';
-
-            Console.WriteLine(number);
-            Console.WriteLine(price);
-            Console.WriteLine(character2);
-            Console.WriteLine(name);
-            Console.WriteLine(isAdmin);
-            Console.WriteLine(price.ToString() + " " + n.ToString());
-            Console.WriteLine(n);
-            Console.WriteLine(character1);
-            Console.WriteLine(character);
+            Console.WriteLine("{0} {1}", byte.MaxValue, byte.MinValue);
+            Console.WriteLine("{0} {1}", int.MaxValue, int.MinValue);
+            Console.WriteLine("{0} {1}", float.MaxValue, float.MinValue);
+            Console.WriteLine("{0} {1}", double.MaxValue, double.MinValue);
         }
     }
 }
