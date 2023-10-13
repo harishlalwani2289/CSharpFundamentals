c1ce88b4c02d80d924d78f42aec5f19983f41c34
Harish Lalwani <harishlalwani2289@gmail.com>
2023-10-13 16:09:15 -0400

    Playing with variables

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -12,6 +12,25 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            byte number = 253;
+            float price = 20.54f;
+            char character = '$';
+            string name = "Harish";
+            bool isAdmin = false;
+            bool isGuest = false;
+            int n = 0;
+            char character1 = 'a';
+            char character2 = 'b';
+
+            Console.WriteLine(number);
+            Console.WriteLine(price);
+            Console.WriteLine(character2);
+            Console.WriteLine(name);
+            Console.WriteLine(isAdmin);
+            Console.WriteLine(price.ToString() + " " + n.ToString());
+            Console.WriteLine(n);
+            Console.WriteLine(character1);
+            Console.WriteLine(character);
         }
     }
 }
